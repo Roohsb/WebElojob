05a2a3a622246928c210a83c985ba686868d8121
Rodrigo S <89086381+Roohsb@users.noreply.github.com>
2021-08-22 07:32:54 -0300

    Update configuracion.php

--- ElojobLoL/Web/configurar/configuracion.php
+++ ElojobLoL/Web/configurar/configuracion.php
@@ -9,12 +9,12 @@ define("link", "http://localhost:81"); //Adidione o link completo
 define("logo", "http://localhost:81/Template/imagens/logo_w.png"); //Adidione o link da logo
 define("whatsapplink", "https://api.whatsapp.com/send?phone=5522981546064&amp;text=Ol%C3%A1,%20vi%20o%20seu%20contato%20no%20site%20da%20ELOJOB%20X");
 //------------------------------------------------------------------------------------------------
-// Configurações GERAL
+// ConfiguraÃ§Ãµes GERAL
 //--------------------------------------------------------------------------------------------------------
 define("titulo", "Sla caralho!"); // titulo
 define("name", "Job6666"); // Nome do seu site
 define("copyright", "6666"); // Texto do copy
-define("whatsapp", "(22) 999004878(Sua mãe)");
+define("whatsapp", "(22) 999004878(A lenda)");
 
 
 ?>
